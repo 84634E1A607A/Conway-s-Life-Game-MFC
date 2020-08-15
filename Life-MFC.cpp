78d16a00f6f990fb2acd2e42b35ac8b195ed3f28
84634E1A607A <60734649+84634E1A607A@users.noreply.github.com>
2020-08-15 19:43:53 +0800

    那个无关紧要的改动不能改...

--- Life-MFC.cpp
+++ Life-MFC.cpp
@@ -173,12 +173,6 @@ void CLifeMFCApp::OnOptionsSave()
 
 void CLifeMFCApp::OnHelpHelp()
 {
-	m_pMainWnd->MessageBox(L"Help:\n\
-		space - start/stop\n\
-		leftclick - select/unselect\n\
-		rightclick - deploy builtin\n\
-		b, 0-3 - select builtin\n\
-		d, 0-7 - select direction\n\
-		Options->C/D - Click two points to create/delete a rectangle region",
+	m_pMainWnd->MessageBox(L"Help:\n\nspace - start/stop\nleftclick - select/unselect\nrightclick - deploy builtin\nb, 0-3 - select builtin\nd, 0-7 - select direction\nOptions->C/D - Click two points to create/delete a rectangle region",
 		L"Help", MB_OK);
 }
